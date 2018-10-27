5cc28c4b07b2cd8348345b7e690705cdbed00a70
wujunze <itwujunze@163.com>
2018-10-27 16:23:40 +0800

    di implement

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wujunze
+ * Date: 2018/10/27
+ * Time: 3:32 PM
+ */
+
+namespace Spark\Framework;
+
+
+class Application
+{
+    public function run ()
+    {
+
+    }
+}
---
+++ src/exceptions/ContainerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wujunze
+ * Date: 2018/10/27
+ * Time: 3:36 PM
+ */
+
+namespace Spark\Framework\Exceptions;
+
+use \Exception;
+
+class ContainerException extends Exception
+{
+
+}
