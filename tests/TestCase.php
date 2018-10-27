a66448ee328d62b40d959d4d66d790f52c9e32f8
wujunze <itwujunze@163.com>
2018-10-27 16:23:53 +0800

    di unit test

---
+++ tests/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wujunze
+ * Date: 2018/10/27
+ * Time: 3:55 PM
+ */
+
+namespace Spark\Framework\Tests;
+
+
+use \PHPUnit\Framework\TestCase as BaseCase;
+
+class TestCase extends BaseCase
+{
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wujunze
+ * Date: 2018/10/27
+ * Time: 3:56 PM
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
